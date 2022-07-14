34bac5e69b22941b6b6addd1b44ba2f6bd4759d7
Arseny <93079612+codedeliveryservice@users.noreply.github.com>
2022-07-14 16:19:24 +0300

    Use JavaScript interpolation

--- src/Web/wwwroot/js/editor.js
+++ src/Web/wwwroot/js/editor.js
@@ -2,7 +2,7 @@
     imageInput.onchange = (event) => {
         const file = imageInput.files[0];
         if (file) {
-            imageContainer.style.backgroundImage = 'url(' + URL.createObjectURL(file) + ')';
+            imageContainer.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
             imageContainer.innerText = '';
         }
     }
